75b08c552a5af3f984c9f8afac5dbd2b1d1e3206
LimaEchoCharlie <LimaEchoCharlie@outlook.com>
2017-09-08 14:34:33 +0100

    Rover project: only trust mapping in the near vision of the rover

--- 1-01-search-and-return/02-rover-project/code/perception.py
+++ 1-01-search-and-return/02-rover-project/code/perception.py
@@ -125,12 +125,12 @@ def orientation_heuristic(Rover):
     return (abs_pitch < 1.0) & (abs_roll < 1.0)
 
 # create an array that will mask out the portions of the image that furthest from the eye
-# the eye is at (max_x/2, max_y)
+# the eye is at (max_x/2, max_y) and the eye is pointing -y
 # mask is applied independently in the x and y direction
-def near_eye_mask(shape, x_factor=0.2, y_factor=0.2):
-    y_limit = int(shape[0]*y_factor)
-    x_limit = int(shape[1]*x_factor/2)
-    mask = np.ones(shape)
+def near_eye_mask(shape, dtype, width_factor=0.2, depth_factor=0.2):
+    y_limit = int(shape[0] * depth_factor)
+    x_limit = int(shape[1] * width_factor / 2)
+    mask = np.ones(shape, dtype=dtype)
     mask[0:y_limit,:] = 0
     mask[:,0:x_limit] = 0
     mask[:,(shape[1]-x_limit):shape[1]] = 0
@@ -159,6 +159,8 @@ def perception_step(Rover):
 
     # 3) Apply color threshold to identify navigable terrain/obstacles/rock samples
     navigable = color_thresh(warped)
+    near_mask = near_eye_mask(navigable.shape, dtype=navigable.dtype, width_factor=0.4, depth_factor=0.3)
+    near_navigable = cv2.bitwise_and( np.copy(navigable), near_mask)
 
     # Obstacles are simply navigable inverted
     mask = perspect_transform(np.ones_like(Rover.img[:,:,0]), source, destination)
@@ -170,27 +172,30 @@ def perception_step(Rover):
     # 4) Update Rover.vision_image (this will be displayed on left side of screen)
     Rover.vision_image[:,:,0] = obstacle*255        # color-thresholded binary image
     Rover.vision_image[:,:,1] = rock_sample*255     # color-thresholded binary image
-    Rover.vision_image[:,:,2] = navigable*255       # terrain color-thresholded binary image
+    Rover.vision_image[:,:,2] = navigable*180       # terrain color-thresholded binary image
+    Rover.vision_image[:,:,2] = near_navigable*255  # terrain color-thresholded binary image
+
 
     # 5) Convert map image pixel values to rover-centric coords
     xpix_navigable, ypix_navigable = rover_coords(navigable)
+    xpix_near_navigable, ypix_near_navigable = rover_coords(near_navigable)
     xpix_obstacles, ypix_obstacles = rover_coords(obstacle)
     xpix_rocks, ypix_rocks = rover_coords(rock_sample)
 
     # 6) Convert rover-centric pixel values to world coordinates
     scale = 2 * dst_size
-    navigable_x_world, navigable_y_world = rover_to_world(xpix_navigable, ypix_navigable, Rover, scale)
+    near_navigable_x_world, near_navigable_y_world = rover_to_world(xpix_near_navigable, ypix_near_navigable, Rover, scale)
     obstacle_x_world, obstacle_y_world = rover_to_world(xpix_obstacles, ypix_obstacles, Rover, scale)
     rock_x_world, rock_y_world = rover_to_world(xpix_rocks, ypix_rocks, Rover, scale)
 
     # 7) Update Rover worldmap (to be displayed on right side of screen)
     if orientation_heuristic(Rover):
         Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] = 255
-        Rover.worldmap[navigable_y_world, navigable_x_world, 2] = 255
+        Rover.worldmap[near_navigable_y_world, near_navigable_x_world, 2] = 255
 
     Rover.worldmap[rock_y_world, rock_x_world, 1] = 255
 
-    frame_fidelity.save_frame(Rover,(navigable_x_world, navigable_y_world))
+    frame_fidelity.save_frame(Rover,(near_navigable_x_world, near_navigable_y_world))
 
     # 8) Convert rover-centric pixel positions to polar coordinates
     # Update Rover pixel distances and angles
